6815979490f8e0aca53f450ff1be91646d634395
盧柏嘉 <lupochia@LuPaul.local>
2017-03-15 00:44:12 +0800

    change email to account name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314155846) do
+ActiveRecord::Schema.define(version: 20170314161228) do
 
   create_table "seminars", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170314155846) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
