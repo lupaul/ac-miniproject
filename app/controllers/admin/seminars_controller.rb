c6b4385779ad21470e149cf77e73a13219b9b343
盧柏嘉 <lupochia@LuPaul.local>
2017-03-19 13:46:39 +0800

    build admin layouts

--- app/controllers/admin/seminars_controller.rb
+++ app/controllers/admin/seminars_controller.rb
@@ -1,6 +1,10 @@
 class Admin::SeminarsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :admin_required
+  # before_action :authenticate_user!
+  # before_action :admin_required
+  load_and_authorize_resource
+  layout "admin"
+  def index
+    @seminars = Seminar.all
+  end
 
-  
 end
